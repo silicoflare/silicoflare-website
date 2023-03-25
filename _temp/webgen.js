637e60489495cd4953125f8ef96fa78845fe49d6
SilicoFlare <100959814+SilicoFlare@users.noreply.github.com>
2023-03-25 10:36:39 +0530

    edited webgen

--- _temp/webgen.js
+++ _temp/webgen.js
@@ -1,27 +1,18 @@
-setTimeout(function () {
-    var t = document.createElement("title");
-    t.innerHTML = "Dynamic HTML";
-    document.head.appendChild(t);
-}, 2000);
+var t = document.createElement("title");
+t.innerHTML = "Dynamic HTML";
+document.head.appendChild(t);
 
 var b = document.body;
 
-setTimeout(function () {
-    var temp = document.createElement("h1");
-    temp.innerHTML = "This is a heading!!";
-    b.append(temp);
-}, 2000);
+var temp = document.createElement("h1");
+temp.innerHTML = "This is a heading!!";
+b.append(temp);
 
-setTimeout(function () {
-    temp = document.createElement("h2");
-    temp.innerHTML = 'Welcome!!';
-    b.append(temp);
-}, 2000);
+temp = document.createElement("h2");
+temp.innerHTML = 'Welcome!!';
+b.append(temp);
 
-setTimeout(function () {
-    var d = document;
-    temp = d.createElement("div");
-    temp.innerHTML = "This webpage has been ENTIRELY constructed piece-by-piece with JavaScript! Can you believe how powerful dynamically generated pages have become??";
-    b.append(temp);
-}, 2000);
-
+var d = document;
+temp = d.createElement("div");
+temp.innerHTML = "This webpage has been ENTIRELY constructed piece-by-piece with JavaScript! Can you believe how powerful dynamically generated pages have become??";
+b.append(temp);
