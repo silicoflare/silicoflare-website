7bbefaa28b2c84604b08b90e07613fc48904e971
Suraj B M <silicoflare@gmail.com>
2024-03-05 15:44:07 +0530

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Portfolio, MainHead, Avatar, Intro, PortSection, SubHead, LinkBar, SocialLink, BulletList, ListItem, MD, Label, CardList, Card } from '@silicoflare/portfolify';
+import { Portfolio, MainHead, Avatar, Intro, PortSection, SubHead, LinkBar, SocialLink, MD, CardList, Card } from '@silicoflare/portfolify';
 
 
 export default function App()   {
